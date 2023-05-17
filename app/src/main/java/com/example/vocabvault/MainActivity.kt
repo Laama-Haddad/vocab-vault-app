c120bd117656d332db24f336ede05b4659d3f241
LaamaHaddad <laama.haddad.it@gmail.com>
2023-05-17 04:13:26 +0400

    Create All Components of Result
    Show Audio in US & UK, Part Of Speech, and first definition

--- app/src/main/java/com/example/vocabvault/MainActivity.kt
+++ app/src/main/java/com/example/vocabvault/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.vocabvault
 
 import CustomAdapter
 import android.content.Context
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.util.Log.d
+import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.ListView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import com.google.android.material.textfield.TextInputEditText
@@ -16,26 +20,54 @@ import java.io.IOException
 class MainActivity : AppCompatActivity(), ResponseCallback {
     lateinit var wordInputText: TextInputEditText
     lateinit var translateBtn: Button
-
+    private lateinit var volumeUKImageView: ImageView
+    private lateinit var volumeUSImageView: ImageView
+    private lateinit var ukTextView: TextView
+    private lateinit var usTextView: TextView
+    private var mediaPlayerUS: MediaPlayer? = null
+    private var mediaPlayerUK: MediaPlayer? = null
+    private var voiceUrlUS: String? = null
+    private var voiceUrlUK: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initializeViews()
         wordInputText.addTextChangedListener {
             translateBtn.isEnabled = !wordInputText.text.toString().trim().isEmpty()
+            if (!wordInputText.text.toString().trim().isEmpty()) {
+                volumeUSImageView.visibility = View.GONE
+                volumeUKImageView.visibility = View.GONE
+                ukTextView.visibility = View.GONE
+                usTextView.visibility = View.GONE
+            }
         }
         translateBtn.setOnClickListener {
             val word = wordInputText.text
             if (word.toString().trim().isNotEmpty()) {
                 fetchWordDetails(word.toString(), this)
+            } else {
+                volumeUKImageView.visibility = View.GONE
+                volumeUSImageView.visibility = View.GONE
+                ukTextView.visibility = View.GONE
+                usTextView.visibility = View.GONE
             }
         }
+        volumeUKImageView.setOnClickListener { onUKIconClick(it, voiceUrlUK!!) }
+        volumeUSImageView.setOnClickListener { onUSIconClick(it, voiceUrlUS!!) }
     }
 
     private fun initializeViews() {
         wordInputText = findViewById(R.id.word_text_input_id)
         translateBtn = findViewById(R.id.translate_button_id)
+        volumeUKImageView = findViewById(R.id.uk_icon_id)
+        volumeUSImageView = findViewById(R.id.us_icon_id)
+        ukTextView = findViewById(R.id.uk_text_view_id)
+        usTextView = findViewById(R.id.us_text_view_id)
         translateBtn.isEnabled = false
+        volumeUSImageView.visibility = View.GONE
+        volumeUKImageView.visibility = View.GONE
+        ukTextView.visibility = View.GONE
+        usTextView.visibility = View.GONE
     }
 
     private fun fetchWordDetails(word: String, callback: ResponseCallback) {
@@ -65,24 +97,100 @@ class MainActivity : AppCompatActivity(), ResponseCallback {
             }
         })
     }
+
     override fun onSuccess(result: ResultOutput) {
         runOnUiThread {
             // UI update code here
             d("MainActivity", "Result: $result")
+            val audioList = result.audio
+            for (item in audioList) {
+                if (item.contains("-us", ignoreCase = true)) voiceUrlUS = item
+                if (item.contains("-uk", ignoreCase = true)) voiceUrlUK = item
+            }
+            if(voiceUrlUK==null){
+                volumeUKImageView.visibility = View.GONE
+                ukTextView.visibility = View.GONE
+            }else{
+                volumeUKImageView.visibility = View.VISIBLE
+                ukTextView.visibility = View.VISIBLE
+            }
+            if(voiceUrlUS==null){
+                volumeUSImageView.visibility = View.GONE
+                usTextView.visibility = View.GONE
+            }else{
+                volumeUSImageView.visibility = View.VISIBLE
+                usTextView.visibility = View.VISIBLE
+            }
             val listView = findViewById<ListView>(R.id.list_view_id)
             val adapter = CustomAdapter(this@MainActivity, result.meanings)
             listView.adapter = adapter
             adapter.notifyDataSetChanged()
-            hideKeyboard(this,wordInputText)
+            hideKeyboard(this, wordInputText)
         }
     }
-
 
     override fun onFailure(error: String) {
         // Handle the failure
     }
+
     private fun hideKeyboard(context: Context, view: TextInputEditText) {
-        val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        val inputMethodManager =
+            context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
+
+    private fun onUSIconClick(view: View, voiceUrl: String) {
+        // Handle icon click event
+        if (mediaPlayerUS == null) {
+            mediaPlayerUS = MediaPlayer()
+            mediaPlayerUS?.setOnCompletionListener {
+                // Release the MediaPlayer resources when playback is completed
+                mediaPlayerUS?.release()
+                mediaPlayerUS = null
+            }
+        } else {
+            mediaPlayerUS?.reset()
+        }
+
+        try {
+            mediaPlayerUS?.setDataSource(voiceUrl)
+            mediaPlayerUS?.prepare()
+            mediaPlayerUS?.start()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            // Handle any errors that occur during MediaPlayer setup or playback
+        }
+    }
+
+    private fun onUKIconClick(view: View, voiceUrl: String) {
+        // Handle icon click event
+        if (mediaPlayerUK == null) {
+            mediaPlayerUK = MediaPlayer()
+            mediaPlayerUK?.setOnCompletionListener {
+                // Release the MediaPlayer resources when playback is completed
+                mediaPlayerUK?.release()
+                mediaPlayerUK = null
+            }
+        } else {
+            mediaPlayerUK?.reset()
+        }
+
+        try {
+            mediaPlayerUK?.setDataSource(voiceUrl)
+            mediaPlayerUK?.prepare()
+            mediaPlayerUK?.start()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            // Handle any errors that occur during MediaPlayer setup or playback
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayerUS?.release()
+        mediaPlayerUS = null
+        mediaPlayerUK?.release()
+        mediaPlayerUK = null
+    }
+
 }
