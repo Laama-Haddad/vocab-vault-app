683cc282e1c558044eac2ed70daf2cde281fd503
LaamaHaddad <laama.haddad.it@gmail.com>
2023-05-19 14:47:48 +0400

    Merge definitions according to part-of-speech

--- app/src/main/java/com/example/vocabvault/Utils.kt
+++ app/src/main/java/com/example/vocabvault/Utils.kt
@@ -12,12 +12,18 @@ class Utils {
             var audioList = phoneticList.map { it }.distinct().toMutableList()
             var meaningList = mutableListOf<MeaningResult>()
             var result: ResultOutput?
-            wordEntries.forEach { it ->
-                for (item in it.meanings) {
-                    var speech = item.partOfSpeech
-                    var defs = item.definitions.map { it }.toMutableList()
-                    var meaningResultItem = MeaningResult(speech, defs)
-                    meaningList.add(meaningResultItem)
+            wordEntries.forEach { wordEntry ->
+                val meaningsByPartOfSpeech = wordEntry.meanings.groupBy { it.partOfSpeech }
+                meaningsByPartOfSpeech.forEach { (partOfSpeech, meanings) ->
+                    val definitions = meanings.flatMap { it.definitions }
+                    val matchingItem = meaningList.find { it.partOfSpeech == partOfSpeech }
+                    if (matchingItem != null) {
+                        matchingItem.definitions.addAll(definitions)
+                    } else {
+                        val meaningResultItem =
+                            MeaningResult(partOfSpeech, definitions.toMutableList())
+                        meaningList.add(meaningResultItem)
+                    }
                 }
             }
             result = ResultOutput(word, audioList, meaningList)
